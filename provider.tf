2be7052cc5de41039c34fb5865eef3cd8d3d85b2
raghav9329 <raghav9329@gmail.com>
2023-02-26 12:48:48 +0530

    removed profile

--- provider.tf
+++ provider.tf
@@ -5,7 +5,7 @@
 provider "aws" {
   region = var.region
   /* shared_credentials_file = "~/.aws/config" */
-  profile = "default"
+  
   assume_role {
     role_arn     = "arn:aws:iam::${var.account_id}:role/AdminRole"
     session_name = "demo"
